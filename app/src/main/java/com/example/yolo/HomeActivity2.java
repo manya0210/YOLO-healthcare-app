b4a3ab443213d851e4bc7c74185be3e35495893c
Manya Bisht <bishtmanya14@gmail.com>
2024-04-18 04:43:34 +0530

    added the app icon

--- app/src/main/java/com/example/yolo/HomeActivity2.java
+++ app/src/main/java/com/example/yolo/HomeActivity2.java
@@ -1,9 +1,12 @@
 package com.example.yolo;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -15,10 +18,34 @@ public class HomeActivity2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_home2);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        CardView findNearbyHospital = findViewById(R.id.findHospital);
+        findNearbyHospital.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(HomeActivity2.this, NearbyHospitalsActivity2.class));
+            }
+        });
+
+        CardView findNearbyPharmacy = findViewById(R.id.findPharmacy);
+        findNearbyPharmacy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(HomeActivity2.this, NearbyPharmacyActivity2.class));
+            }
+        });
+
+        CardView askQueries = findViewById(R.id.chatbot);
+        askQueries.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(HomeActivity2.this, askQueriesChatbotActivity2.class));
+            }
         });
     }
 }
